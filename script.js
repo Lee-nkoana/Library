bef083537be8a72e6c73e2156b2d40a5c2b61ff4
Lee-nkoana <lebonkoana6@gmail.com>
2024-02-27 13:07:26 +0200

    Function to add books to array created

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.bookinfo = function(){
-        return(this.title, this.author, this.pages, this.read);
+    this.bookInfo = function(){
+        return {title, author, pages, read};
     };
 }
 
+function addBookToLibrary(book){
+    myLibrary.push(book);
+}
+
 const book1 = new Book('LOTR', 'JK Rollings', '132', 'not read');
-console.log(book1.bookinfo)
+const book2 = new Book('The pig house', 'Jackie', '243', 'read');
+const book3 = new Book('spade', 'Linc Snide', '600', 'not read');
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+
+for (const book of myLibrary) {
+    console.log(book.bookInfo());
+}
